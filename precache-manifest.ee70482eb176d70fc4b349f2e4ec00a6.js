780ce65ecc8f1feda89583955d7d5aecbbce83a0
Akif Gultekin <mehmetgultekin@sabanciuniv.edu>
2020-05-25 02:46:58 +0300

    Updates

---
+++ precache-manifest.ee70482eb176d70fc4b349f2e4ec00a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1bdb0e891ba97058c443c5f51fba9be",
+    "url": "/SecureTificate/index.html"
+  },
+  {
+    "revision": "f5f0dcfead8803d46540",
+    "url": "/SecureTificate/static/css/main.ef193001.chunk.css"
+  },
+  {
+    "revision": "4009baa364d4058897b8",
+    "url": "/SecureTificate/static/js/2.22456324.chunk.js"
+  },
+  {
+    "revision": "f5f0dcfead8803d46540",
+    "url": "/SecureTificate/static/js/main.6791a173.chunk.js"
+  },
+  {
+    "revision": "0e372ba1f47c26eb71e2",
+    "url": "/SecureTificate/static/js/runtime-main.edf1c38e.js"
+  }
+]);
