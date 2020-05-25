36c6d3325d8f78215064e66570d680d45b84b76b
Akif Gultekin <mehmetgultekin@sabanciuniv.edu>
2020-05-25 03:23:54 +0300

    solved:404 when reupload

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Toolbar from './Components/Toolbar/Toolbar'
 import './App.css'
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch ,HashRouter } from 'react-router-dom'
 import BackGround from './Components/Background/Background'
 import "./App.css";
 import Check from "./Components/Check/Check";
@@ -10,17 +10,18 @@ import Home from "./Components/Home/Home";
 
 class App extends Component {
   render() {
-    return (
+    return (<HashRouter basename="/">
       <div className="App">
       <Toolbar className="ToolBar"/>
         <Switch>
-            <Route className="Check"  path="/SecureTificate/check" exact component={()=><Check backgroundColor= "rgb(219,68,55)"/>} />
-            <Route className="Home"  path="/SecureTificate/house" exact component={()=><House  backgroundColor="rgb(242,183,5)" />} />
-            <Route className="House"  path="/SecureTificate" exact component={()=><Home backgroundColor= "rgb(4,173,191)"/>} />
+            <Route className="Check"  path="/check" exact component={()=><Check backgroundColor= "rgb(219,68,55)"/>} />
+            <Route className="Home"  path="/house" exact component={()=><House  backgroundColor="rgb(242,183,5)" />} />
+            <Route className="House"  path="/" exact component={()=><Home backgroundColor= "rgb(4,173,191)"/>} />
             <Route className="House"  component={()=><Home backgroundColor= "rgb(4,173,191)"/>} />
         </Switch>
         <BackGround className="BackGround" style={{width:'90%',height:window.innerHeight}}></BackGround>
       </div>
+      </HashRouter>
     );
   }
 }
--- client/src/Components/Button/Button.js
+++ client/src/Components/Button/Button.js
@@ -5,7 +5,7 @@ const Button = (props) => {
     console.log(props.top)
     return (
         <button
-            className="CustomButton" style={{ left: props.left, right: props.right, top: props.top, backgroundColor: props.BG, width: props.width, maxWidth: props.maxWidth }}
+            className="CustomButton" type="button" style={{ left: props.left, right: props.right, top: props.top, backgroundColor: props.BG, width: props.width, maxWidth: props.maxWidth }}
             onClick={(e) => props.Clicked(e, props.value)}
         >{props.value} {props.children}</button>
     )
