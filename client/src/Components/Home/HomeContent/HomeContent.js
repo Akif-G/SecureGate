78f5bed988d4b5af5dfce9f424dae04503d57238
Akif Gultekin <mehmetgultekin@sabanciuniv.edu>
2020-05-26 03:16:39 +0300

    dark mode fully usable!

--- client/src/Components/Home/HomeContent/HomeContent.js
+++ client/src/Components/Home/HomeContent/HomeContent.js
@@ -7,7 +7,7 @@ class HomeContent extends Component {
                 <p className="Information" style={{top:"17rem",fontSize:"1.8rem"}}>SecureTificate is a Secure Certification Factory, powered on Ethereum Blockchain...</p>
                 <p className="Information" style={{top:"28rem",fontSize:"1.4rem"}}>You can interact with it via web3 components such as Metamask.</p>
                 <p className="Information" style={{top:"35rem",fontSize:"1.4rem"}}>SecureTificate is running on Ropsten Test Network. Since it has a developing interface we do not recommend it for commercial usage.</p>
-                <p className="Information" style={{top:"43rem",fontSize:"1rem"}}>We are trying to develop bug-free and refreshing interface for the ease of usage of a contract Dapp. If you'd like to help us, see our code or the contract details; you can reach us by clicking to the github icon.</p>
+                <p className="Information" style={{top:"45rem",fontSize:"0.8rem"}}>We are trying to develop bug-free and refreshing interface for the ease of usage of a contract Dapp. If you'd like to help us, see our code or the contract details; you can reach us by clicking to the github icon.</p>
 
             </div>
         )
--- client/src/Components/Social/Social.js
+++ client/src/Components/Social/Social.js
@@ -3,12 +3,15 @@ import './Social.css'
 //import github from './github.png'
 const Social = (props) => {
     var mode="ModeWhite"
+    var invertStyle=null;
     if(props.mode=="black"){
-        var mode="ModeBlack"
+        mode="ModeBlack"
+        invertStyle="invert(1)"
     }
+
     return (
         <Fragment>
-            <a style={{ right: "3em" }} href="https://github.com/Akif-G/SecureTificate" className="InteractiveButton" id="Github"></a>
+            <a style={{ right: "3em" ,filter:invertStyle}} href="https://github.com/Akif-G/SecureTificate" className="InteractiveButton" id="Github"></a>
             <div style={{ right: "8em" }} onClick={props.ChangeMode} className="InteractiveButton" id={mode} ></div>
         </Fragment>
     )
