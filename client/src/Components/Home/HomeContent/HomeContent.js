7c68131c88ebd35aefa8ba636907036fb14a9011
Akif Gultekin <mehmetgultekin@sabanciuniv.edu>
2020-07-19 18:34:49 +0300

    changed link color

--- client/src/Components/Home/HomeContent/HomeContent.js
+++ client/src/Components/Home/HomeContent/HomeContent.js
@@ -10,7 +10,7 @@ class HomeContent extends Component {
                 <p className="Information" style={{ top: "17rem", fontSize: "1.8rem" }}>A Secure Gate to enter your Certification House!</p>
                 <p className="Information" style={{ top: "23rem", fontSize: "1.4rem" }}>SecureGate is a Secure Certification Factory, powered on Ethereum Blockchain...You can interact with it via web3 components such as Metamask.</p>
                 <p className="Information" style={{ top: "30rem", fontSize: "1.4rem" }}>SecureGate is running on Ropsten Test Network. Since it is still under construction interface we do not recommend it for commercial usage. Our address:</p>
-                <p className="Information" style={{ top: "37rem", fontSize: "1.2rem" }}>Smart Contract Address:: 0x054292065bbec2e6ed374f85e8a938f05f5f2f48</p>
+                <p className="Information" style={{ top: "37rem", fontSize: "1.2rem" }}>Smart Contract Address:: <a style={{ display: "table-cell" }} href="https://ropsten.etherscan.io/address/0x054292065bbec2e6ed374f85e8a938f05f5f2f48" target="_blank">0x054292065bbec2e6ed374f85e8a938f05f5f2f48</a></p>
                 <p className="Information" style={{ top: "42rem", fontSize: "1rem" }}>We are trying to develop bug-free and refreshing interface for the ease of use of a contract Dapp. If you'd like to help us, see our code or the contract details; you can reach us by clicking to the github icon.</p>
             </div>
         )
